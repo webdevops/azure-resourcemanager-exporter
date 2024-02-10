d22625826b1d4c0f24974e0081db0523a8bfccc4
Markus Blaschke <mblaschke82@gmail.com>
2024-02-10 13:03:25 +0100

    lowercase collector name

--- main.go
+++ main.go
@@ -243,7 +243,7 @@ func initMetricCollector() {
 		logger.With(zap.String("collector", collectorName)).Infof("collector disabled")
 	}
 
-	collectorName = "Reservation"
+	collectorName = "reservation"
 	if Config.Collectors.Reservation.IsEnabled() {
 		c := collector.New(collectorName, &MetricsCollectorAzureRmReservation{}, logger)
 		c.SetScapeTime(*Config.Collectors.Reservation.ScrapeTime)
